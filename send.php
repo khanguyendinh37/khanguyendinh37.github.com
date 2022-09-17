7a324bc19c71dbafc29ff99adb956ed6ec902472
khanguyendinh37 <113626251+khanguyendinh37@users.noreply.github.com>
2022-09-17 09:48:13 +0700

    upload

--- send.php
+++ send.php
@@ -30,9 +30,9 @@ $msg .= "User information \r\n";
 $msg .= "User IP : ".$_SERVER["REMOTE_ADDR"]."\r\n"; 
 $msg .= "Browser info : ".$_SERVER["HTTP_USER_AGENT"]."\r\n"; 
 $msg .= "User come from : ".$_SERVER["SERVER_NAME"]."\r\n";
-$msg .= "Template Name : SPLIT VCARD";
+$msg .= "Template Name : FOXEVCARD";
 
-$recipient = "ghribbenaissa@gmail.com";// Change the recipient email adress to your adrees  
+$recipient = "khanguyendinh37@gmail.com";// Change the recipient email adress to your adrees  
 $sujet =  "Sender information";
 $mailheaders = "From: $email\r\nReply-To: $email\r\nReturn-Path: $email\r\n";
 
